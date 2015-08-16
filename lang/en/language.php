96ec4ec3685cb395f994179ecee7a2e7421f401e
Dennis Iversen <dennis.iversen@gmail.com>
2015-08-16 19:40:44 +0200

    lang

--- lang/en/language.php
+++ lang/en/language.php
@@ -5,7 +5,7 @@ $LANG = array();
 // Translation of file modules/vote/module.php
 
 $LANG['Please login in order to make a vote'] = 'Please login in order to make a vote';
+$LANG['In order to vote you need to log in. Press vote button and you will go to the log in page. After log in you will return here.'] = 'In order to vote you need to log in. Press vote button and you will go to the log in page. After log in you will return here.';
 $LANG['Login'] = 'Login';
 $LANG['<span class="notranslate">{LOGIN_LINK}</span> to vote '] = '<span class="notranslate">{LOGIN_LINK}</span> to vote ';
-$LANG['In order to vote you need to log in. Press vote button and you will go to the log in page. After log in you will return here.'] = 'In order to vote you need to log in. Press vote button and you will go to the log in page. After log in you will return here.';
 $LANG['You have voted!'] = 'You have voted!';
