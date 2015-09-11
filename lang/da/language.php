448c93ede61bba818ddc27943bb0c2ebd79416eb
Dennis Iversen <dennis.iversen@gmail.com>
2015-09-11 15:02:18 +0200

    lang

--- lang/da/language.php
+++ lang/da/language.php
@@ -4,6 +4,5 @@ $LANG = array();
 
 $LANG['Please login in order to make a vote'] = 'Venligst login for at gøre en afstemning';
 $LANG['In order to vote you need to log in. Press vote button and you will go to the log in page. After log in you will return here.'] = 'For at stemme skal du logge ind Tryk stemme knappen., Og du vil gå til login siden. Efter log ind, vil du vende tilbage her.';
-$LANG['Login'] = 'Log på';
-$LANG['<span class="notranslate">{LOGIN_LINK}</span> to vote '] = '<span class="notranslate">{LOGIN_LINK}</span> til at stemme';
+$LANG['Something went wrong! Try again later'] = 'Noget gik galt! Prøv igen senere';
 $LANG['You have voted!'] = 'Du har stemt!';
--- lang/en/language.php
+++ lang/en/language.php
@@ -6,6 +6,5 @@ $LANG = array();
 
 $LANG['Please login in order to make a vote'] = 'Please login in order to make a vote';
 $LANG['In order to vote you need to log in. Press vote button and you will go to the log in page. After log in you will return here.'] = 'In order to vote you need to log in. Press vote button and you will go to the log in page. After log in you will return here.';
-$LANG['Login'] = 'Login';
-$LANG['<span class="notranslate">{LOGIN_LINK}</span> to vote '] = '<span class="notranslate">{LOGIN_LINK}</span> to vote ';
+$LANG['Something went wrong! Try again later'] = 'Something went wrong! Try again later';
 $LANG['You have voted!'] = 'You have voted!';
